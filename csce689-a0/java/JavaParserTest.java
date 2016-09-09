e05740a20721f5d256bd9f85297e745349d5d4d9
sidxz <sid@tamu.edu>
2016-09-09 03:01:07 -0500

    Assignment0:  Variable extraction for part1

--- csce689-a0/java/JavaParserTest.java
+++ csce689-a0/java/JavaParserTest.java
@@ -1,4 +1,6 @@
 import java.io.IOException;
+import java.util.*;
+
 
 import org.antlr.v4.runtime.ANTLRFileStream;
 import org.antlr.v4.runtime.CharStream;
@@ -7,6 +9,8 @@ import org.antlr.v4.runtime.tree.ParseTree;
 import org.antlr.v4.runtime.tree.ParseTreeWalker;
 
 public class JavaParserTest extends JavaBaseListener{
+
+	static List<String> variables;
 
 	public static void main(String[] args) throws IOException {
 		if(args.length<1)
@@ -24,15 +28,40 @@ public class JavaParserTest extends JavaBaseListener{
     	ParseTree tree = parser.compilationUnit();
     	ParseTreeWalker walker = new ParseTreeWalker(); // create standard walker
     	JavaParserTest listener = new JavaParserTest(); // create a parse tree listener
+
+    	variables = new ArrayList<>();
+    	
     	walker.walk(listener, tree); // traverse parse tree with listener
     }
+
 
 	/**
 	 * {@inheritDoc}
 	 */
-	// @Override
-	// public void enterStatement(JavaParser.StatementContext ctx){
-	// 	System.out.println("enterStatement");
-	//     //your code starts here
-	// }
+	@Override
+	public void enterStatement(JavaParser.StatementContext ctx){
+		//System.out.println("enterStatement");
+
+		if(ctx.getStart().getText().equals("if")) {
+    	//first token is 'if'
+			
+			String child = ctx.getChild(1).getText().replace("(", "").replace(")", "");
+			if(variables.contains(child) && child.length() > 3) {
+				System.out.println(child + " " +ctx.getStart().getLine());
+			}
+
+			//System.out.println("getRuleCOntext() -> "+ctx.getRuleContext().getText());
+			//System.out.println("getRuleCOntext() -> "+ctx.getCause().getText());
+			/*if(ctx.getChild(1) instanceof IfStmtContext) {
+				System.out.println("This");
+			}*/
+		}	
+	}
+	 @Override
+	public void enterVariableDeclarator(JavaParser.VariableDeclaratorContext ctx) {
+		//variables.add('('+ctx.variableDeclaratorId().getText()+')');
+		variables.add(ctx.variableDeclaratorId().getText());
+
+               // System.out.println("Variable ---> "+ctx.variableDeclaratorId().getText());
+            }
 }
