f41933614980b6951c441a56dc9d04c764078514
sidxz <sid@tamu.edu>
2016-09-11 11:52:57 -0500

    csce689-a0: PART 3 Complete

--- csce689-a0/java/ModifyIfListner.java
+++ csce689-a0/java/ModifyIfListner.java
@@ -18,25 +18,26 @@ public class ModifyIfListner extends JavaBaseListener {
 
     @Override
     public void enterStatement(JavaParser.StatementContext ctx) {
-        /*
-        Filter statements starting with if
-        */
+    /*
+    Filter statements starting with if
+    */
     if(ctx.getStart().getText().equals("if")) {
         /*
         Select only variables without ! and length >3
         */
         String child = ctx.getChild(1).getText().replace("(", "").replace(")", "");
-          if(variables.contains(child) && child.length() > 3) {
+
+        if(variables.contains(child) && child.length() > 3) {
 
             String field = "  { \n\t\t\t\tSystem.out.println(\""+child+" "+ctx.getStart().getLine()+"\");\n\t\t\t\t\b";
             int ix = ctx.start.getTokenIndex();
             while(!(rewriter.getTokenStream().get(ix).getText().equals(")"))) {
-            ix++;
+                ix++;
             }
             rewriter.insertAfter(rewriter.getTokenStream().get(ix), field);
             rewriter.insertAfter(ctx.getStop(), "\n\t\t\t}");
-          }
-        
+            }
+
         }
     }   
     
